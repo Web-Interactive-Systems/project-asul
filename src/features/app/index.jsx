4b504eada13df3088b6310ea612a09e980071b46
Mohamed <ezzaouia.mohamed@gmail.com>
2023-10-09 15:08:51 +0200

    adding radix-themes

---
+++ src/features/app/index.jsx
@@ -0,0 +1,11 @@
+import { Theme } from "@radix-ui/themes";
+
+import "@radix-ui/themes/styles.css";
+
+export function App() {
+  return (
+    <Theme>
+      <h1>React App</h1>
+    </Theme>
+  );
+}
