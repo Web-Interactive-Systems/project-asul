079d7a722d07863a880df9ca699f23a5111b58af
Mohamed <ezzaouia.mohamed@gmail.com>
2023-10-09 09:45:34 +0200

    initial commit

---
+++ src/actions/getMatches.js
@@ -0,0 +1,17 @@
+import { supabase } from "../lib/supabase";
+
+export async function getMatches() {
+  //
+  const { data, error } = await supabase.from("Match").select();
+
+  console.log("getMatches", data, error);
+
+  if (error) {
+    console.error("error", error);
+    throw Error(error.message);
+  }
+
+  console.log("success", data);
+
+  return data;
+}
