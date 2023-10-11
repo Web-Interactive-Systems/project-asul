aa3bcac7407885599b2097259e30e8135c33c787
Lolom <lorenzoaoulini@gmail.com>
2023-10-11 12:20:28 +0200

    WIP: dashboard plot

--- src/components/Plot.jsx
+++ src/components/Plot.jsx
@@ -1,5 +1,24 @@
 import * as Plot from "@observablehq/plot";
+import Data from "@lib/data.json";
 
 export default function PlotFigure({ options }) {
-  return Plot.plot({ ...options, document: new Document() }).toHyperScript();
+  const marks = [
+    Plot.dot(Data[0].scores, {
+      x: "date",
+      y: "score",
+    }),
+  ];
+
+  // Data.forEach((user) => {
+  const dot = Plot.dot(Data[0].scores, {
+    x: "date",
+    y: "score",
+  });
+  // marks.push(dot);
+  console.log(marks);
+  console.log(options);
+  options.marks = marks;
+  const plot = Plot.plot({ ...options, document: new Document() });
+  console.log(plot);
+  return plot.toHyperScript();
 }
