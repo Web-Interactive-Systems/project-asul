cf181c25bdbe4bb6aeae492e1e6fac5544cc6ccf
Charlelie <charleliegros@gmail.com>
2023-10-11 17:52:27 +0200

    wip

--- src/features/score/index.jsx
+++ src/features/score/index.jsx
@@ -1,55 +1,87 @@
-import { Button, Flex, Separator, Strong, TextField } from "@radix-ui/themes";
+import {
+  Button,
+  Flex,
+  Heading,
+  Separator,
+  Strong,
+  TextField,
+  Callout,
+} from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 
 export function Score({ J2 = { nom: "J2" } }) {
+  const [scoreJ1, setScoreJ1] = useState(0);
+  const [scoreJ2, setScoreJ2] = useState(0);
+  const [isScoresValid, setIsScoresValid] = useState(true);
+  useEffect(() => {
+    if (scoreJ1 < 0 || scoreJ2 < 0 || scoreJ1 === "" || scoreJ2 === "") {
+      setIsScoresValid(false);
+    } else setIsScoresValid(true);
+  }, [scoreJ1, scoreJ2]);
   const handleClick = (event) => {
-    console.log(event);
+    console.log(`score 1 : ${scoreJ1}`);
+    console.log(`score 2 : ${scoreJ2}`);
   };
   return (
-    <Flex
-      direction="column"
-      align="center"
-      gap="5"
-    >
+    <>
       <Flex
         direction="column"
         align="center"
-        gap="3"
+        gap="5"
       >
-        <TextField.Root radius="large">
-          <TextField.Slot>J1</TextField.Slot>
-          <TextField.Input
-            type="number"
-            min="0"
-            onKeyDownCapture={(event) => {
-              (event.charCode != 8 && event.charCode == 0) ||
-                (event.charCode >= 48 && event.charCode <= 57);
-            }}
-            placeholder="Score de J1"
-          />
-        </TextField.Root>
+        <Heading>Score</Heading>
         <Flex
-          gap="2"
+          direction="column"
           align="center"
+          gap="3"
         >
-          <Separator size="2" />
-          <Strong>Vs</Strong>
-          <Separator size="2" />
+          <TextField.Root radius="large">
+            <TextField.Slot>J1</TextField.Slot>
+            <TextField.Input
+              type="number"
+              min="0"
+              name="scoreJ1"
+              value={scoreJ1}
+              onChange={(e) => setScoreJ1(e.target.value)}
+              placeholder="Score de J1"
+            />
+          </TextField.Root>
+          <Flex
+            gap="2"
+            align="center"
+          >
+            <Separator size="2" />
+            <Strong>Vs</Strong>
+            <Separator size="2" />
+          </Flex>
+          <TextField.Root radius="large">
+            <TextField.Slot>{J2.nom}</TextField.Slot>
+            <TextField.Input
+              type="number"
+              name="scoreJ2"
+              value={scoreJ2}
+              onChange={(e) => setScoreJ2(e.target.value)}
+              placeholder={`Score de ${J2.nom}`}
+            />
+          </TextField.Root>
         </Flex>
-        <TextField.Root radius="large">
-          <TextField.Slot>{J2.nom}</TextField.Slot>
-          <TextField.Input
-            type="number"
-            placeholder={`Score de ${J2.nom}`}
-          />
-        </TextField.Root>
+        <Button
+          radius="large"
+          color="grass"
+          onClick={(e) => handleClick(e)}
+          disabled={!isScoresValid}
+        >
+          Valider le score
+        </Button>
       </Flex>
-      <Button
-        radius="large"
-        color="grass"
-        onClick={handleClick}
+      <Callout.Root
+        size="1"
+        color="red"
+        style={{ position: "absolute", bottom: "0" }}
       >
-        Valider le score
-      </Button>
-    </Flex>
+        <Callout.Icon></Callout.Icon>
+        <Callout.Text size="1">Les scores ne sont pas valides.</Callout.Text>
+      </Callout.Root>
+    </>
   );
 }
