63a6ff4f4ed23103235ac5881881ab4fa51a0386
Louen12 <129731619+Louen12@users.noreply.github.com>
2023-10-12 09:23:27 +0200

    v4

--- src/features/Match/CreateMatch.jsx
+++ src/features/Match/CreateMatch.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Flex, Heading, Box, Popover, Button } from "@radix-ui/themes"
+import {useState} from "react";
+import { Flex, Heading, Box, Button, Text, Strong, Separator } from "@radix-ui/themes"
 import {
   Combobox,
   ComboboxInput,
@@ -12,44 +13,60 @@ import {
 import { usePlayerMatch } from "../Hooks/usePlayerMatch";
 
 
+
 function InuputSelect({ placeholder = "input select", onSelect }) {
 
-  const [term, setTerm] = React.useState("");
+  const [term, setTerm] = useState("");
+
+  
 
   const results = usePlayerMatch(term);
 
   const handleChange = (event) => setTerm(event.target.value);
 
+
+
   return (
     <Combobox aria-labelledby="demo" onSelect={onSelect} openOnFocus={true}>
       <ComboboxInput autocomplete
         onChange={handleChange}/>
-<ComboboxPopover>
+      <ComboboxPopover>
         <Box>
-          
           <ComboboxList>
             {results.map((data) => (
                <ComboboxOption key={data.name} value={data.name} />
             ))}
           </ComboboxList>
         </Box>
-  </ComboboxPopover>
+      </ComboboxPopover>
     </Combobox>
-
   )
 }
 
 
 export function CreateMatch() {
-  const handleSelect = (selection) => console.log("handleSelect", selection)
+  const [DisableButton, setDisableButton] = useState(false);
 
+  const handleSelect = (selection) => {
+    if (selection == ""){
+      setDisableButton(false)
+    }
+    else{
+      setDisableButton(true)
+    }
+  }
+  
   return (
-    <Flex direction="column">
+    <Flex direction="column" align="center" gap="3">
       <Heading>Create Match</Heading>
-      <Flex direction="row">
-
-        <InuputSelect placeholder="Player 1" onSelect={handleSelect} />
+      <Text as="span"> <Strong>Moi </Strong> </Text>
+      <Flex direction="row" align="center" >
+        <Separator orientation="horizontal" size="3" />
+        <Text as="span"> VS </Text>
+        <Separator orientation="horizontal" size="3" />
       </Flex>
+      <InuputSelect placeholder="Player 1" onSelect={handleSelect} />
+      <Button radius="large" color="blue" variant="solid" disabled={DisableButton}>Envoyer une demande de Match</Button>
     </Flex>
 
   );
