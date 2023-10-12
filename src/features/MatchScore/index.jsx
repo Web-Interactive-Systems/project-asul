74e0792312ff27cbc932de1398b9a14097f56acf
Charlelie <charleliegros@gmail.com>
2023-10-12 09:43:06 +0200

    match score

--- src/features/MatchScore/index.jsx
+++ src/features/MatchScore/index.jsx
@@ -5,22 +5,36 @@ import {
   Separator,
   Strong,
   TextField,
-  Callout,
+  Text,
 } from "@radix-ui/themes";
 import { useEffect, useState } from "react";
 
-export function Score({ J2 = { nom: "J2" } }) {
+export function MatchScore({ J2 = { name: "J2" } }) {
   const [scoreJ1, setScoreJ1] = useState(0);
   const [scoreJ2, setScoreJ2] = useState(0);
   const [isScoresValid, setIsScoresValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(" ");
+
   useEffect(() => {
     if (scoreJ1 < 0 || scoreJ2 < 0 || scoreJ1 === "" || scoreJ2 === "") {
+      scoreJ1 < 0 || scoreJ2 < 0
+        ? setErrorMessage("Un score doit être positif.")
+        : scoreJ1 === "" || scoreJ2 === ""
+        ? setErrorMessage("Un score ne peut pas être vide.")
+        : null;
       setIsScoresValid(false);
-    } else setIsScoresValid(true);
+    } else {
+      setIsScoresValid(true);
+      setErrorMessage(" ");
+    }
   }, [scoreJ1, scoreJ2]);
+
   const handleClick = (event) => {
-    console.log(`score 1 : ${scoreJ1}`);
-    console.log(`score 2 : ${scoreJ2}`);
+    scoreJ1 == 0 && scoreJ2 == 0
+      ? setErrorMessage("Les deux scores ne peuvent pas être nuls.")
+      : null;
+    console.log(`score J1 : ${scoreJ1}`);
+    console.log(`score J2 : ${scoreJ2}`);
   };
   return (
     <>
@@ -34,6 +48,7 @@ export function Score({ J2 = { nom: "J2" } }) {
           direction="column"
           align="center"
           gap="3"
+          style={{ maxWidth: "200px" }}
         >
           <TextField.Root radius="large">
             <TextField.Slot>J1</TextField.Slot>
@@ -49,39 +64,48 @@ export function Score({ J2 = { nom: "J2" } }) {
           <Flex
             gap="2"
             align="center"
+            style={{ maxWidth: "200px" }}
           >
             <Separator size="2" />
             <Strong>Vs</Strong>
             <Separator size="2" />
           </Flex>
           <TextField.Root radius="large">
-            <TextField.Slot>{J2.nom}</TextField.Slot>
+            <TextField.Slot>{J2.name}</TextField.Slot>
             <TextField.Input
               type="number"
               name="scoreJ2"
               value={scoreJ2}
               onChange={(e) => setScoreJ2(e.target.value)}
-              placeholder={`Score de ${J2.nom}`}
+              placeholder={`Score de ${J2.name}`}
             />
           </TextField.Root>
         </Flex>
-        <Button
-          radius="large"
-          color="grass"
-          onClick={(e) => handleClick(e)}
-          disabled={!isScoresValid}
+        <Flex
+          direction="column"
+          align="center"
+          gap="2"
+          width="100%"
         >
-          Valider le score
-        </Button>
+          <Button
+            style={{ maxWidth: "200px", width: "100%" }}
+            radius="large"
+            color="grass"
+            onClick={(e) => handleClick(e)}
+            disabled={!isScoresValid}
+          >
+            Valider le score
+          </Button>
+          <Text
+            color="red"
+            size="1"
+            align="center"
+            style={{ maxWidth: "75%" }}
+          >
+            {errorMessage}
+          </Text>
+        </Flex>
       </Flex>
-      <Callout.Root
-        size="1"
-        color="red"
-        style={{ position: "absolute", bottom: "0" }}
-      >
-        <Callout.Icon></Callout.Icon>
-        <Callout.Text size="1">Les scores ne sont pas valides.</Callout.Text>
-      </Callout.Root>
     </>
   );
 }
