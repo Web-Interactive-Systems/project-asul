47f172026201c7680162c9d0f22e4ccbcdfa6091
Lolom <lorenzoaoulini@gmail.com>
2023-10-13 11:06:55 +0200

    Multiples marks

--- src/features/dashboard/Plot.jsx
+++ src/features/dashboard/Plot.jsx
@@ -5,15 +5,18 @@ import * as Plot from "@observablehq/plot";
  * @param {
  *  {
  *    plotOptions: any,
- *    type: string,
+ *    type: string[],
  *    data: any[],
  *    marksOptions: any
  *  }
  * } param0
  * @returns
  */
-export default function PlotFigure({ plotOptions, type, data, marksOptions }) {
-  const marks = [Plot[type](data, marksOptions)];
+export default function PlotFigure({ plotOptions, types, data, marksOptions }) {
+  const marks = types.map((type) => {
+    return Plot[type](data, marksOptions[type]);
+  });
+  console.log(marks);
   const plot = Plot.plot({
     ...plotOptions,
     marks,
