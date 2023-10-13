01425ea7f8dc2a2130baee9cc6fb50643da29c1e
Lolom <lorenzoaoulini@gmail.com>
2023-10-13 10:37:45 +0200

    plot type

--- src/features/dashboard/Plot.jsx
+++ src/features/dashboard/Plot.jsx
@@ -5,47 +5,14 @@ import * as Plot from "@observablehq/plot";
  * @param {
  *  {
  *    plotOptions: any,
- *    type: "area" | "arrow" | "auto" | "axis" | "bar" | "bollinger" | "box" | "cell" | "contour" | "delaunay" | "density" | "dot" | "frame" | "geo" | "grid" | "hexgrid" | "image" | "line" | "linearRegression" | "link" | "raster" | "rect" | "rule" | "text" | "tick" | "tip" | "tree" | "vector,
+ *    type: string,
  *    data: any[],
  *    marksOptions: any
  *  }
  * } param0
  * @returns
  */
-const types = [
-  "area",
-  "arrow",
-  "auto",
-  "axis",
-  "bar",
-  "bollinger",
-  "box",
-  "cell",
-  "contour",
-  "delaunay",
-  "density",
-  "dot",
-  "frame",
-  "geo",
-  "grid",
-  "hexgrid",
-  "image",
-  "line",
-  "linearRegression",
-  "link",
-  "raster",
-  "rect",
-  "rule",
-  "text",
-  "tick",
-  "tip",
-  "tree",
-  "vector",
-];
 export default function PlotFigure({ plotOptions, type, data, marksOptions }) {
-  if (!types.includes(type)) {
-    throw new Error(`Plot type must be one of ${types.join(", ")}`);
-  }
   const marks = [Plot[type](data, marksOptions)];
   const plot = Plot.plot({
     ...plotOptions,
