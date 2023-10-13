32a2e6ddf976f4ed069bd3cf40508012103d7a2e
Lolom <lorenzoaoulini@gmail.com>
2023-10-13 10:29:43 +0200

    Types validation

--- src/features/dashboard/Plot.jsx
+++ src/features/dashboard/Plot.jsx
@@ -12,7 +12,40 @@ import * as Plot from "@observablehq/plot";
  * } param0
  * @returns
  */
+const types = [
+  "area",
+  "arrow",
+  "auto",
+  "axis",
+  "bar",
+  "bollinger",
+  "box",
+  "cell",
+  "contour",
+  "delaunay",
+  "density",
+  "dot",
+  "frame",
+  "geo",
+  "grid",
+  "hexgrid",
+  "image",
+  "line",
+  "linearRegression",
+  "link",
+  "raster",
+  "rect",
+  "rule",
+  "text",
+  "tick",
+  "tip",
+  "tree",
+  "vector",
+];
 export default function PlotFigure({ plotOptions, type, data, marksOptions }) {
+  if (!types.includes(type)) {
+    throw new Error(`Plot type must be one of ${types.join(", ")}`);
+  }
   const marks = [Plot[type](data, marksOptions)];
   const plot = Plot.plot({
     ...plotOptions,
