47b3b3e486c2713500670e8cf8bc6eb36cef7794
Lolom <lorenzoaoulini@gmail.com>
2023-10-20 11:03:03 +0200

    Account

--- src/features/account/Account.jsx
+++ src/features/account/Account.jsx
@@ -3,12 +3,12 @@ import sessionStore from '@/store/session';
 import { useStore } from '@nanostores/react';
 import { MatchList } from '../matchlist/MatchList';
 import { SessionList } from '../matchlist/SessionList';
-
 import { useState } from 'react';
+
 export function Account() {
   const userSession = useStore(sessionStore);
-  const [isMatchList, setIsMatchList] = useState(false)
-  const [session, setSession] = useState(null)
+  const [isMatchList, setIsMatchList] = useState(false);
+  const [session, setSession] = useState(null);
   const params = new URLSearchParams(window.location.search);
   return (
     <Tabs.Root defaultValue={params.get('init') ? 'profile' : 'match'}>
@@ -20,17 +20,24 @@ export function Account() {
 
       <Box px="4" pt="3" pb="2">
         <Tabs.Content value="match">
-          {isMatchList &&
-          <MatchList onClose={() => {
-            setIsMatchList(false)
-          }} session={session} /> ||
-          <SessionList onClose={(s) => {
-            setSession(s);
-            setIsMatchList(true);
-          }} />}
+          {(isMatchList && (
+            <MatchList
+              onClose={() => {
+                setIsMatchList(false);
+              }}
+              session={session}
+            />
+          )) || (
+            <SessionList
+              onClose={(s) => {
+                setSession(s);
+                setIsMatchList(true);
+              }}
+            />
+          )}
         </Tabs.Content>
 
-        <Tabs.Content value="dashboard">  
+        <Tabs.Content value="dashboard">
           <Text size="2">Access and update your documents.</Text>
         </Tabs.Content>
 
