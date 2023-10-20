6da078fbb0ae7403522e30bde87121e1177faa74
Laura <106605150+LauraR40@users.noreply.github.com>
2023-10-20 15:29:39 +0200

    page barem
    ajout de fonction pour manipuler barem

--- src/features/barem/Barem.jsx
+++ src/features/barem/Barem.jsx
@@ -1,45 +1,44 @@
 import { Table, TextArea, Box, TextFieldInput } from '@radix-ui/themes';
+import { getGrades } from '@/actions/getGrades';
+import { useState, useEffect } from 'react';
+
+
 export function Barem() {
+    const[grades, setGrades] = useState([]);
+
+    useEffect(async () => {
+      console.log('useEffect Barem start')
+        const response = await getGrades();
+        console.log('useEffect Barem start', response)
+        setGrades(response.data);
+    }, [])
+        
 
     return (
 
     <Table.Root>
   <Table.Header>
-    <Table.Row>
+   
+       
       <Table.ColumnHeaderCell>Points d'écart entre victoire et défaite (divisés par 10)</Table.ColumnHeaderCell>
       <Table.ColumnHeaderCell>Coef applicable</Table.ColumnHeaderCell>
-    </Table.Row>
+    
   </Table.Header>
 
   <Table.Body>
-    <Table.Row>
-      <Table.RowHeaderCell>Pour -20 à -12</Table.RowHeaderCell>
-      <Table.RowHeaderCell><TextFieldInput size="1" placeholder="-0.4" type='number' step='0.1' /></Table.RowHeaderCell>
-    </Table.Row>
 
-    <Table.Row>
-      <Table.RowHeaderCell>Pour -11 à -7</Table.RowHeaderCell>
-      <Table.RowHeaderCell><TextArea size="1" placeholder="-0.2" /></Table.RowHeaderCell>
-    </Table.Row>
+  {grades.map(e =>(
+            <Table.Row>
+              <Table.RowHeaderCell>Pour {e.min} à {e.max}</Table.RowHeaderCell>
+      <Table.RowHeaderCell>
+        <TextFieldInput size="1" placeholder={e.coef} type='number' step='0.1' />
+      </Table.RowHeaderCell>
+            </Table.Row>
+        ))}
 
-    <Table.Row>
-      <Table.RowHeaderCell>Pour -6 à -1</Table.RowHeaderCell>
-      <Table.RowHeaderCell><TextArea size="1" placeholder="0" /></Table.RowHeaderCell>
-    </Table.Row>
-    
-    <Table.Row>
-      <Table.RowHeaderCell>Pour 0 à 6</Table.RowHeaderCell>
-      <Table.RowHeaderCell><TextArea size="1" placeholder="0.7" /></Table.RowHeaderCell>
-    </Table.Row>
-    <Table.Row>
-      <Table.RowHeaderCell>Pour 7 à 11</Table.RowHeaderCell>
-      <Table.RowHeaderCell><TextArea size="1" placeholder="1.3" /></Table.RowHeaderCell>
-    </Table.Row>
-    <Table.Row>
-      <Table.RowHeaderCell>Pour 12 à 20</Table.RowHeaderCell>
-      <Table.RowHeaderCell><TextArea size="1" placeholder="1.6" /></Table.RowHeaderCell>
-    </Table.Row>
   </Table.Body>
 </Table.Root>
+
+
     )
 }
