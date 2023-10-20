cdd51971e8e9814c22ce1464dae4897cbaee5a68
Mohamed <ezzaouia.mohamed@gmail.com>
2023-10-20 16:49:21 +0200

    integrating grades and players

--- src/features/barem/Barem.jsx
+++ src/features/barem/Barem.jsx
@@ -1,44 +1,39 @@
-import { Table, TextArea, Box, TextFieldInput } from '@radix-ui/themes';
+import { Table, TextFieldInput } from '@radix-ui/themes';
 import { getGrades } from '@/actions/getGrades';
 import { useState, useEffect } from 'react';
-
+import { supabase } from '@/lib/supabase';
 
 export function Barem() {
-    const[grades, setGrades] = useState([]);
+  const [grades, setGrades] = useState([]);
 
-    useEffect(async () => {
-      console.log('useEffect Barem start')
-        const response = await getGrades();
-        console.log('useEffect Barem start', response)
-        setGrades(response.data);
-    }, [])
-        
+  useEffect(() => {
+    (async () => {
+      const res = await getGrades();
+      setGrades(res.data);
+    })();
+  }, []);
 
-    return (
-
+  return (
     <Table.Root>
-  <Table.Header>
-   
-       
-      <Table.ColumnHeaderCell>Points d'écart entre victoire et défaite (divisés par 10)</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell>Coef applicable</Table.ColumnHeaderCell>
-    
-  </Table.Header>
+      <Table.Header>
+        <Table.ColumnHeaderCell>
+          Points d'écart entre victoire et défaite (divisés par 10)
+        </Table.ColumnHeaderCell>
+        <Table.ColumnHeaderCell>Coef applicable</Table.ColumnHeaderCell>
+      </Table.Header>
 
-  <Table.Body>
-
-  {grades.map(e =>(
-            <Table.Row>
-              <Table.RowHeaderCell>Pour {e.min} à {e.max}</Table.RowHeaderCell>
-      <Table.RowHeaderCell>
-        <TextFieldInput size="1" placeholder={e.coef} type='number' step='0.1' />
-      </Table.RowHeaderCell>
-            </Table.Row>
+      <Table.Body>
+        {grades.map((e) => (
+          <Table.Row key={e.id}>
+            <Table.RowHeaderCell>
+              Pour {e.min} à {e.max}
+            </Table.RowHeaderCell>
+            <Table.RowHeaderCell>
+              <TextFieldInput size="1" placeholder={e.coef} type="number" step="0.1" />
+            </Table.RowHeaderCell>
+          </Table.Row>
         ))}
-
-  </Table.Body>
-</Table.Root>
-
-
-    )
+      </Table.Body>
+    </Table.Root>
+  );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,25 @@
-import { atom } from 'nanostores';
+import { atom, task, onMount } from 'nanostores';
+import { getPlayers } from '@/actions/getPlayers';
 
 export const $matchContent = atom('session');
 export const $matchSession = atom({});
+export const $players = atom([]);
+
+onMount($players, () => {
+  task(async () => {
+    const { data, error } = await getPlayers();
+
+    if (error) {
+      //
+      console.error('$getPlayers', error);
+    } else {
+      $players.set(data);
+
+      console.warn('$getPlayers', $players.get());
+    }
+  });
+});
+
+// export async function $getPlayers() {
+
+// }
