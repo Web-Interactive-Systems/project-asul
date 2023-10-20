4a50ca85b9ae4decab7444f25ed7d09ae2264b30
tanguyleboss <71895709+tanguyleboss@users.noreply.github.com>
2023-10-20 10:50:25 +0200

    dashboard Etudiant + getNbMatchById

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 export { getMatches } from './getMatches';
 export { getAuthUser } from './getAuthUser';
 export { getPlayers } from './getPlayers';
+export { getNbMatchById } from './getNbMatchById';
