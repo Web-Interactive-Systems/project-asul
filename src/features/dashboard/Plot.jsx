9061317610c4384628f2c2bfc75343437b16af60
LaPoloche <buchon.paul@gmail.com>
2023-10-24 15:22:52 +0200

    tes

--- src/features/dashboard/Plot.jsx
+++ src/features/dashboard/Plot.jsx
@@ -57,7 +57,12 @@ export default new Proxy(
         const data = useContext(DataContext);
 
         useEffect(() => {
-          setMarks((prev) => [...prev, Plot[name](data, options)]);
+          if (name === "ruleY") {
+            setMarks((prev) => [...prev, Plot[name](options)])
+          }
+          else{
+            setMarks((prev) => [...prev, Plot[name](data, options)]);
+          }
         }, [data]);
 
         return null;
