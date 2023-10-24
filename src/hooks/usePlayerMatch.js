ef01bbc4b62176be49ced525ac7b449a8486e2cf
Louen12 <129731619+Louen12@users.noreply.github.com>
2023-10-24 18:53:23 +0200

    plus de match contre soit même

--- src/hooks/usePlayerMatch.js
+++ src/hooks/usePlayerMatch.js
@@ -4,23 +4,24 @@ import { matchSorter } from 'match-sorter';
 import { useThrottle } from './useThrottle';
 import { $players } from '@/store/store';
 import { useStore } from '@nanostores/react';
+import { $userSession } from '@/store/store';
 
 // const players = $players; //[{name:"Toto"},{name:"Foo"},{name:"Bar"}]
 
 export function usePlayerMatch(term) {
-  let players = useStore($players);
+  const players = useStore($players);
+  const userSession = useStore($userSession);
+  const filteredPlayers = players.filter((player) => player.id !== userSession.player.id)
   let throttledTerm = useThrottle(term, 100);
-
-  console.log('pplayers __', players, $players.get());
 
   return React.useMemo(
     () =>
       term.trim() === ''
-        ? players
-        : matchSorter(players, term, {
+        ? filteredPlayers
+        : matchSorter(filteredPlayers, term, {
             keys: ['username'],
           }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [throttledTerm, players]
+    [throttledTerm, filteredPlayers]
   );
 }
