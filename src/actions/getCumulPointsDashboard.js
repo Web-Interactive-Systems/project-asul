d1ea2626e983860984b985f932b6876a626d6ff4
LaPoloche <buchon.paul@gmail.com>
2023-10-24 15:11:11 +0200

    pour rebase

--- src/actions/getCumulPointsDashboard.js
+++ src/actions/getCumulPointsDashboard.js
@@ -1,48 +1,66 @@
-import { getScores } from "./getScore"
+import { getScores } from './getScore';
+import { getPlayers } from './getPlayers';
 
-function getBonus(delta){
-  let bonus = delta/10
-  if(delta <= -12){
-    bonus += -0.4
-  } else if(delta <= -7){
-    bonus += -0.2
-  } else if(delta <= -1){
-    bonus += 0
-  } else if(delta <= 6){
-    bonus += 0.7
-  } else if(delta <= 11){
-    bonus += 1.3
-  } else if(delta <= 20){
-    bonus += 1.6
+function getBonus(delta) {
+  let bonus = delta / 10;
+  if (delta <= -12) {
+    bonus += -0.4;
+  } else if (delta <= -7) {
+    bonus += -0.2;
+  } else if (delta <= -1) {
+    bonus += 0;
+  } else if (delta <= 6) {
+    bonus += 0.7;
+  } else if (delta <= 11) {
+    bonus += 1.3;
+  } else if (delta <= 20) {
+    bonus += 1.6;
   }
-  return Math.round(bonus*100)/100
+  return Math.round(bonus * 100) / 100;
 }
 
-export async function getCumulPointsDashboard(){
-  const{data,error} = await getScores();
-  let last_score = {}
-  let score = []
-  if(!error){
-    data.forEach((el) =>{
-      if(el != {}){
-        if(!last_score[el["winer_id"]]){
-          last_score[el["winer_id"]] = 100
+export async function getCumulPointsDashboard() {
+  const { data, error } = await getScores();
+
+  const { dataP, errorP } = await getPlayers();
+  
+  console.log(dataP)
+  let last_score = {};
+  let score = [];
+  if (!error) {
+    data.forEach((el) => {
+      if (el != {}) {
+        if (!last_score[el['winer_id']]) {
+          last_score[el['winer_id']] = 100;
         }
-        if(!last_score[el["loser_id"]]){
-          last_score[el["loser_id"]] = 100
+        if (!last_score[el['loser_id']]) {
+          last_score[el['loser_id']] = 100;
         }
       }
-    })
-    data.forEach((el) =>{
-      let delta = (el["winer_score"] - el["loser_score"])
-      last_score[el["winer_id"]] = last_score[el["winer_id"]] + getBonus(delta)
-      last_score[el["loser_id"]] = last_score[el["loser_id"]] + getBonus(-delta)
-      score.push({"userid":el["winer_id"],"score":last_score[el["winer_id"]],"match_date":new Date(el["created_at"])})
-      score.push({"userid":el["loser_id"],"score":last_score[el["loser_id"]],"match_date":new Date(el["created_at"])})
-    })
-    // console.table(last_score)
-    // console.table(score)
-    // score.sort((a, b) => (new Date(a["created_at"] > new Date(b["created_at"])) ? 1 : -1))
+    });
+    data.forEach((el) => {
+      let delta = el['winer_score'] - el['loser_score'];
+      last_score[el['winer_id']] = last_score[el['winer_id']] + getBonus(delta);
+      last_score[el['loser_id']] = last_score[el['loser_id']] + getBonus(-delta);
+      let victory = 'Victoire (↗' + getBonus(delta) + ')';
+      let defeat = 'Victoire (↘' + getBonus(-delta) + ')';
+      score.push({
+        Joueur: el['winer_id'],
+        adversaire: el['loser_id'],
+        result: victory,
+        score: last_score[el['winer_id']],
+        match_date:el['created_at'],
+        match: el['winer_score'] + ' - ' + el['loser_score'],
+      });
+      score.push({
+        Joueur: el['loser_id'],
+        adversaire: el['winer_id'],
+        result: defeat,
+        score: last_score[el['loser_id']],
+        match_date: el['created_at'],
+        match: el['loser_score'] + ' - ' + el['winer_score'],
+      });
+    });
   }
-  return(score)
+  return score;
 }
